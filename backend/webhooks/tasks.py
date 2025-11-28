6c5d1a09f32bfa21b026ab5554793ea15e4bbe7b
adhish1478 <adhish.aravind7@gmail.com>
2025-11-29 03:06:55 +0530

    feat(webhooks): implement Celery tasks for webhook sending and event broadcasting

---
+++ backend/webhooks/tasks.py
@@ -0,0 +1,31 @@
+import requests
+import time
+from celery import shared_task
+from .models import Webhook
+
+@shared_task(bind=True)
+def send_webhook(self, webhook_id, event, payload):
+    wh= Webhook.objects.get(pk= webhook_id)
+    if not wh.enabled:
+        return {"status": "Webhook disabled"}
+    
+    body= {
+        "event": event,
+        "payload": payload
+    }
+
+    start= time.time()
+
+    try:
+        response= requests.post(wh.url, json= body, timeout= 15)
+        elapsed= int(time.time() - start) * 1000 # in ms
+        return {"status_code": response.status_code, "response_time_ms": elapsed, "response_body": response.text}
+    except Exception as exc:
+        # retry
+        raise self.retry(exc= exc, countdown= 60, max_retries= 3)
+    
+@shared_task
+def trigger_event_webhook(event, payload):
+    matches= Webhook.objects.filter(event= event, enabled= True)
+    for wh in matches:
+        send_webhook.apply_async((wh.id, event, payload), queue='webhooks')
