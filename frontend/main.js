14f620772b1a0412ceb9b84712011f2294fa833c
adhish1478 <adhish.aravind7@gmail.com>
2025-11-30 02:32:20 +0530

    fix: https

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,7 @@
 // main.js - Frontend for Product Importer
 // Configure your API base URL here:
 //const API_BASE = (window.API_BASE || '').replace(/\/$/, '') || "http://localhost:8000/api";
-const API_BASE = 'http://34.100.164.253:8000/api';
+const API_BASE = 'https://34.100.164.253/api';
 //
 // Simple API helpers
 //
